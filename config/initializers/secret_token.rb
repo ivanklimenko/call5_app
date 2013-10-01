83589b826a8b823eb1c2159cb01a80e27cf3a449
Ivan Klimenko <ivan.klimenko@gmail.com>
2013-10-02 02:40:15 +0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Call5App::Application.config.secret_key_base = 'af84f1cb00cfb3fafe38cac37f3189f612d79ba9ebcd2a45c2d32addfc6fea147e2b8b28a19911ecd674c6367d249748b969067fd00d2ab6df3cb00a314cb769'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Call5App::Application.config.session_store :cookie_store, key: '_call5_app_session'
